478e58cbce7e64c52491375288258f1180a02b34
Piet Eckhart <pieteckhart@gmail.com>
2020-12-01 21:34:20 +0100

    Day #1.1

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expenseReport = File.ReadAllLines("input1.txt").Select(i => int.Parse(i)).ToList();
+
+            for (int i = 0; i < expenseReport.Count; i++)
+            {
+                int entry = expenseReport[i];
+                for (int i1 = i+1; i1 < expenseReport.Count; i1++)
+                {
+                    int otherEntry = expenseReport[i1];
+                    if (entry + otherEntry == 2020)
+                    {
+                        Console.WriteLine(entry * otherEntry);
+                        goto hoi;
+                    }
+                }
+            }
+        hoi:
+            Console.WriteLine("Klaar");
+        }
+    }
+}
