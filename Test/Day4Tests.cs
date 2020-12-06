9db4f5d254ec27ef5bef38ac8a75f34b84e6dc7b
Simone <skamer@winbase.nl>
2020-12-07 00:25:10 +0100

    day4 losse validatie voorbeelden

---
+++ Test/Day4Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using Advent;
+using Xunit;
+
+
+namespace Test
+{
+    public class Day4Tests
+    {
+        [Fact]
+        public void Valid()
+        {
+            var input = @"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm";
+
+            var validCount = new APS(input).GetValidCount();
+
+            Assert.Equal(1, validCount);
+        }
+
+        [Fact]
+        public void NoHeight()
+        {
+            var input = @"iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929";
+
+            var validCount = new APS(input).GetValidCount();
+
+            Assert.Equal(0, validCount);
+        }
+
+        [Fact]
+        public void NoCID()
+        {
+            var input = @"hcl:#ae17e1 iyr:2013
+        eyr:2024
+        ecl:brn pid:760753108 byr:1931
+        hgt:179cm";
+
+            var validCount = new APS(input).GetValidCount();
+
+            Assert.Equal(1, validCount);
+        }
+
+        [Fact]
+        public void AlsoInvalid()
+        {
+            var input = @"hcl:#cfa07d eyr:2025 pid:166559648
+        iyr:2011 ecl:brn hgt:59in";
+
+            var validCount = new APS(input).GetValidCount();
+
+            Assert.Equal(0, validCount);
+        }
+    }
+}
---
+++ src/Day4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Advent
+{
+    public class APS
+    {
+        string regex = @"(\w{3}):([\w+#]+)";
+        private string input;
+
+        public APS(string input)
+        {
+            this.input = input;
+        }
+
+        public int GetValidCount()
+        {
+            var result = Regex.Matches(input, regex).Select(m => new {key = m.Groups[1].Value, value = m.Groups[2].Value});
+            var passport = new
+            {
+                byr = result.FirstOrDefault(g => g.key == "byr"),
+                iyr = result.FirstOrDefault(g => g.key == "iyr"),
+                eyr = result.FirstOrDefault(g => g.key == "eyr"),
+                hgt = result.FirstOrDefault(g => g.key == "hgt"),
+                hcl = result.FirstOrDefault(g => g.key == "hcl"),
+                ecl = result.FirstOrDefault(g => g.key == "ecl"),
+                pid = result.FirstOrDefault(g => g.key == "pid"),
+                cid = result.FirstOrDefault(g => g.key == "cid"),
+            };
+
+            return      passport.byr != null 
+                     && passport.iyr != null
+                     && passport.eyr != null
+                     && passport.hgt != null 
+                     && passport.hcl != null 
+                     && passport.ecl != null 
+                     && passport.pid != null 
+                ? 1 : 0;
+        }
+    }
+}
